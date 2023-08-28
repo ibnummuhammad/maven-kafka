11701fd248fdaf1f01424d41c1f7d43526d5bd20
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-08-28 17:57:59 +0700

    Add KafkaProducerExample.java

--- src/main/java/com/mycompany/producer/KafkaProducerExample.java
+++ src/main/java/com/mycompany/producer/KafkaProducerExample.java
@@ -1,7 +1,16 @@
 package com.mycompany.producer;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class KafkaProducerExample {
+    private static final Logger logger = LogManager
+            .getLogger(KafkaProducerExample.class);
+
     public static void main(String... args) {
         System.out.println("kafkaproducer println");
+        logger.info("kafkaProducer logger");
+
+        System.out.println("Done adding kafkaProducer!");
     }
 }
