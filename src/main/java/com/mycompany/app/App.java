89a85fd2b31cba0fcfa2fc1645de5fa892057ca7
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-08-28 16:29:06 +0700

    Add maven-assembly-plugin

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,15 +1,21 @@
 package com.mycompany.app;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 /**
  * Hello world!
  *
  */
 public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main(String[] args) {
         String EXAMPLE_GOAL = System.getenv("EXAMPLE_GOAL") != null
                 ? System.getenv("EXAMPLE_GOAL")
                 : "producer";
 
+        logger.info("Kafka Topic:");
         System.out.println("Done!");
     }
 }
