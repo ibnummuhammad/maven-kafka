3bc8c2ba5fe805c31984664a766b551584a27fd5
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-08-31 20:06:26 +0700

    Add createTopic()

--- src/main/java/com/mycompany/producer/KafkaProducerExample.java
+++ src/main/java/com/mycompany/producer/KafkaProducerExample.java
@@ -2,11 +2,15 @@ package com.mycompany.producer;
 
 import com.mycompany.commons.Commons;
 
+import kafka.admin.AdminUtils;
+import kafka.admin.RackAwareMode;
 import kafka.utils.ZkUtils;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.ZkConnection;
+
+import java.util.Properties;
 
 public class KafkaProducerExample {
     private static final Logger logger = LogManager
@@ -30,6 +34,20 @@ public class KafkaProducerExample {
         boolean isSecureKafkaCluster = false;
         ZkUtils zkUtils = new ZkUtils(zkClient, new ZkConnection(Commons.EXAMPLE_ZOOKEEPER_SERVER),
                 isSecureKafkaCluster);
+
+        int partitions = 1;
+        int replication = 1;
+
+        Properties topicConfig = new Properties();
+        if (!AdminUtils.topicExists(zkUtils, Commons.EXAMPLE_KAFKA_TOPIC)) {
+            AdminUtils.createTopic(zkUtils, Commons.EXAMPLE_KAFKA_TOPIC, partitions, replication, topicConfig,
+                    RackAwareMode.Safe$.MODULE$);
+            logger.info("Topic {} created.", Commons.EXAMPLE_KAFKA_TOPIC);
+        } else {
+            logger.info("Topic {} already exists.", Commons.EXAMPLE_KAFKA_TOPIC);
+        }
+
+        zkClient.close();
 
         System.out.println("Done add ZkUtils!");
     }
