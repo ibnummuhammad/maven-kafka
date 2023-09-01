ec6b9cc8c36b71ede16def74bcdaaf180559a801
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-09-01 16:14:37 +0700

    Add try catch

--- src/main/java/com/mycompany/producer/KafkaProducerExample.java
+++ src/main/java/com/mycompany/producer/KafkaProducerExample.java
@@ -34,6 +34,21 @@ public class KafkaProducerExample {
         int EXAMPLE_PRODUCER_INTERVAL = System.getenv("EXAMPLE_PRODUCER_INTERVAL") != null
                 ? Integer.parseInt(System.getenv("EXAMPLE_PRODUCER_INTERVAL"))
                 : 100;
+
+        // array of size 4.
+        int[] arr = new int[4];
+        try {
+            int i = arr[4];
+
+            // this statement will never execute
+            // as exception is raised by above statement
+            System.out.println("Inside try block");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Exception caught in Catch block");
+        }
+
+        // rest program will be executed
+        System.out.println("Outside try-catch clause");
     }
 
     private static Producer<String, String> createProducer() {
