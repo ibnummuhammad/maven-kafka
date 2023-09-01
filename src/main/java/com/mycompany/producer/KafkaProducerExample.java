eff9d379b546ac9ee2dcfeed8385a02fc7ad3cb9
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-09-01 16:19:56 +0700

    Fix createTopic()

--- src/main/java/com/mycompany/producer/KafkaProducerExample.java
+++ src/main/java/com/mycompany/producer/KafkaProducerExample.java
@@ -27,35 +27,31 @@ public class KafkaProducerExample {
         int sessionTimeoutMs = 10 * 1000;
         int connectionTimeoutMs = 8 * 1000;
 
-        ZkClient zkClient = new ZkClient(Commons.EXAMPLE_ZOOKEEPER_SERVER, sessionTimeoutMs, connectionTimeoutMs,
-                ZKStringSerializer$.MODULE$);
-        System.out.println("ini zkClient");
-        System.out.println(zkClient);
+        ZkClient zkClient = new ZkClient(Commons.EXAMPLE_ZOOKEEPER_SERVER,
+                sessionTimeoutMs, connectionTimeoutMs, ZKStringSerializer$.MODULE$);
 
         boolean isSecureKafkaCluster = false;
-        ZkUtils zkUtils = new ZkUtils(zkClient, new ZkConnection(Commons.EXAMPLE_ZOOKEEPER_SERVER),
+        ZkUtils zkUtils = new ZkUtils(zkClient,
+                new ZkConnection(Commons.EXAMPLE_ZOOKEEPER_SERVER),
                 isSecureKafkaCluster);
-        System.out.println("ini zkUtils");
-        System.out.println(zkUtils);
 
         int partitions = 1;
         int replication = 1;
 
         Properties topicConfig = new Properties();
-        System.out.println("mau masuk topicExists");
         if (!AdminUtils.topicExists(zkUtils, Commons.EXAMPLE_KAFKA_TOPIC)) {
-            System.out.println("mau masuk createTopic");
-            AdminUtils.createTopic(zkUtils, Commons.EXAMPLE_KAFKA_TOPIC, partitions, replication, topicConfig,
-                    RackAwareMode.Safe$.MODULE$);
+            AdminUtils.createTopic(zkUtils, Commons.EXAMPLE_KAFKA_TOPIC, partitions,
+                    replication, topicConfig, RackAwareMode.Safe$.MODULE$);
             logger.info("Topic {} created.", Commons.EXAMPLE_KAFKA_TOPIC);
             System.out.printf("this Topic %s created.", Commons.EXAMPLE_KAFKA_TOPIC);
         } else {
             logger.info("Topic {} already exists.", Commons.EXAMPLE_KAFKA_TOPIC);
-            System.out.printf("this Topic %s already exists.", Commons.EXAMPLE_KAFKA_TOPIC);
+            System.out.printf("this Topic %s already exists.",
+                    Commons.EXAMPLE_KAFKA_TOPIC);
         }
 
         zkClient.close();
 
-        System.out.println("Done add ZkUtils!");
+        System.out.println("Done createTopic()!");
     }
 }
